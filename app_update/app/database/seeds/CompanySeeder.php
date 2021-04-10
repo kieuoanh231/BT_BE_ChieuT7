e0115679d569167997ce37f58eaa9f567b3c81b0
cms <80998461+kieuoanh231@users.noreply.github.com>
2021-04-10 22:25:36 +0700

    update again because the file is missing when Pushing

---
+++ app_update/app/database/seeds/CompanySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $queryCompanies='insert into companies value (null,?,?,?,?,?)';
+        for ($i = 0; $i < 1000000; $i++) {
+            DB::insert($queryCompanies,['a','a','a','a','a']);
+            // DB::table('companies')->insert([
+            //     'company_name' => 'a',
+            //     'company_web' => 'aa',
+            //     'company_address' => 'a',
+            //     'company_code' => 'a',
+            //     'company_phone' => 'a'
+            // ]);
+        }
+    }
+}
